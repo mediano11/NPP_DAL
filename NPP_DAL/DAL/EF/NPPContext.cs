d7582fd0285321f9d9119d3dcb19f959d9e35a8c
Dmitriy <pest2811@gmail.com>
2023-12-09 01:09:40 +0200

    Add EF Core ORM

---
+++ NPP_DAL/DAL/EF/NPPContext.cs
@@ -0,0 +1,22 @@
+﻿using DAL.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DAL.EF
+{
+    public class NPPContext
+        : DbContext
+    {
+        public DbSet<NPP> Npps { get; set; }
+        public DbSet<Employee> Employees { get; set; }
+        public DbSet<Request> Requests { get; set; }
+
+        public NPPContext(DbContextOptions options)
+            : base(options)
+        {
+        }
+
+    }
+}
