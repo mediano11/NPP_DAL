f36ec217a62ca46b59e58b6d8f188b640b168347
Dmitriy <pest2811@gmail.com>
2023-12-08 23:56:05 +0200

    Add entities to DAL

---
+++ NPP_DAL/DAL/Entities/Employee.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Entities
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime BirthDate { get; set; }
+        public string Gender { get; set; }
+        public string Login { get; set; }
+        public string DepartmentTitle { get; set; }
+        public string PositionTitle { get; set; }
+        public string PositionDescription { get; set; }
+        public int NPPId { get; set; }
+
+        public IEnumerable<Request> Requests { get; set; }
+    }
+}
---
+++ NPP_DAL/DAL/Entities/Request.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Entities
+{
+    public class Request
+    {
+        public int Id { get; set; }
+        public RequestType Type { get; set; }
+        public string Reason { get; set; }
+        public bool IsVoucher { get; set; }
+        public DateTime DurationFrom { get; set; }
+        public DateTime DurationTo { get; set; }
+        public int EmployeeId { get; set; }
+        public Employee Employee { get; set; }
+    }
+
+    public enum RequestType
+    {
+        Annual,
+        Study,
+        Social,
+        Creative,
+        Unpaid
+    }
+}
