9369bb2addd0880840a57e3645267adc4b902f66
Dmitriy <pest2811@gmail.com>
2023-12-16 01:55:29 +0200

    Add DTO

---
+++ NPP_DAL/BLL/DTO/RequestDTO.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL.DTO
+{
+    public class RequestDTO
+    {
+        public int Id { get; set; }
+        public RequestType Type { get; set; }
+        public string Reason { get; set; }
+        public bool IsVoucher { get; set; }
+        public DateTime DurationFrom { get; set; }
+        public DateTime DurationTo { get; set; }
+    }
+
+    public enum RequestType
+    {
+        Annual,
+        Study,
+        Social,
+        Creative,
+        Unpaid
+    }
+}
