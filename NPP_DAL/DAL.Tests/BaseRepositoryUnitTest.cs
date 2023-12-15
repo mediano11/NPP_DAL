ebc8b01d64b3fbf259382e0a31a12ed41333ebf5
Dmitriy <pest2811@gmail.com>
2023-12-15 23:42:11 +0200

    add unit tests for create/delete/get operations

--- NPP_DAL/DAL.Tests/BaseRepositoryUnitTest.cs
+++ NPP_DAL/DAL.Tests/BaseRepositoryUnitTest.cs
@@ -22,8 +22,95 @@ namespace DAL.Tests
 
     public class BaseRepositoryUnitTests
     {
-        
 
+        [Fact]
+        public void Create_InputEmployeeInstance_CalledAddMethodOfDBSetWithEmployeeInstance()
+        {
+            // Arrange
+            DbContextOptions opt = new DbContextOptionsBuilder<NPPContext>().Options;
+            var mockContext = new Mock<NPPContext>(opt);
+            var mockDbSet = new Mock<DbSet<Employee>>();
+            mockContext
+                .Setup(context =>
+                    context.Set<Employee>(
+                        ))
+                .Returns(mockDbSet.Object);
 
+            var repository = new TestEmployeeRepository(mockContext.Object);
+
+            Employee expectedEmployee = new Mock<Employee>().Object;
+
+            //Act
+            repository.Create(expectedEmployee);
+
+            // Assert
+            mockDbSet.Verify(
+                dbSet => dbSet.Add(
+                    expectedEmployee
+                    ), Times.Once());
+        }
+
+        [Fact]
+        public void Delete_InputId_CalledFindAndRemoveMethodsOfDBSetWithCorrectArg()
+        {
+            // Arrange
+            DbContextOptions opt = new DbContextOptionsBuilder<NPPContext>()
+                .Options;
+            var mockContext = new Mock<NPPContext>(opt);
+            var mockDbSet = new Mock<DbSet<Employee>>();
+            mockContext
+                .Setup(context =>
+                    context.Set<Employee>(
+                        ))
+                .Returns(mockDbSet.Object);
+
+            var repository = new TestEmployeeRepository(mockContext.Object);
+
+            Employee expectedEmployee = new Employee() { Id = 1 };
+            mockDbSet.Setup(mock => mock.Find(expectedEmployee.Id)).Returns(expectedEmployee);
+
+            //Act
+            repository.Delete(expectedEmployee.Id);
+
+            // Assert
+            mockDbSet.Verify(
+                dbSet => dbSet.Find(
+                    expectedEmployee.Id
+                    ), Times.Once());
+            mockDbSet.Verify(
+                dbSet => dbSet.Remove(
+                    expectedEmployee
+                    ), Times.Once());
+        }
+
+        [Fact]
+        public void Get_InputId_CalledFindMethodOfDBSetWithCorrectId()
+        {
+            // Arrange
+            DbContextOptions opt = new DbContextOptionsBuilder<NPPContext>()
+                .Options;
+            var mockContext = new Mock<NPPContext>(opt);
+            var mockDbSet = new Mock<DbSet<Employee>>();
+            mockContext
+                .Setup(context =>
+                    context.Set<Employee>(
+                        ))
+                .Returns(mockDbSet.Object);
+
+            Employee expectedEmployee = new Employee() { Id = 1 };
+            mockDbSet.Setup(mock => mock.Find(expectedEmployee.Id))
+                    .Returns(expectedEmployee);
+            var repository = new TestEmployeeRepository(mockContext.Object);
+
+            //Act
+            var actualStreet = repository.Get(expectedEmployee.Id);
+
+            // Assert
+            mockDbSet.Verify(
+                dbSet => dbSet.Find(
+                    expectedEmployee.Id
+                    ), Times.Once());
+            Assert.Equal(expectedEmployee, actualStreet);
+        }
     }
 }
